3287a83e5954e3f69082c0796a1d8ac9bef628f3
Marcus Tan <marcus.m.tan@gmail.com>
2015-08-21 02:41:42 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Galbraith Society Website
+- Server: Node, Express
+- Templating: Jade
+- Styles: Sass
+- Build System: Gulp
+
+# Development Requirements
+- Git
+- NodeJS (0.12.X or newer preferred)
+- Bower
+- Heroku Toolbelt
+
+# Development Set Up
+- `git clone` the repo
+- `npm install` to install node modules (make sure your `NODE_ENV` is set to development and not production or you will not install `devDependencies` in `package.json`)
+- `bower install` to install bower packages
+- run `gulp serve` and make changes to the website as needed
+- when finished run `gulp build` again just to make sure
+- once finished `git commit` to the origin remote
+- when you want to push the website live `git commit` to the heroku remote
+
+# Commands
+- `gulp build` to `clean` all existing `dist` files and `build` from scratch
+- `gulp serve` to host a local server that will serve the website and refresh itself automatically when a file is changed (via BrowserSync)
---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+var gulp 	= require('gulp');
+var path 	= require('path');
+var bsync 	= require('browser-sync').create();
+var reload 	= bsync.reload;
+var $ 		= require('gulp-load-plugins')({ rename: {
+                    'gulp-minify-css':'cssmin',
+                    'gulp-scss-lint':'scsslint'
+                } });
+
+gulp.task('clean', function () {
+	return gulp.src(['dist'], {read: false})
+		.pipe($.clean());
+});
+
+gulp.task('styles', function () {
+	return gulp.src('app/sass/**/*.scss')
+        .pipe($.plumber())    
+		.pipe($.scsslint())
+		.pipe($.sass())
+        .pipe($.concat('app.css'))
+		.pipe($.cssmin())
+		.pipe(gulp.dest('dist/assets'))
+        .pipe(reload({stream:true}));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src('app/scripts/**/*.js')
+        .pipe($.plumber())
+        .pipe($.jshint())
+        .pipe($.jshint.reporter(require('jshint-stylish')))
+        .pipe($.concat('app.js'))
+        .pipe($.uglify())
+        .pipe(gulp.dest('dist/assets'))
+        .pipe(reload({stream:true}));
+});
+
+gulp.task('images', function () {
+	return gulp.src('app/images/**/*')
+        .pipe($.imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        }))
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('fonts', function () {
+    return gulp.src('app/**/fonts/**/*.{eot,svg,ttf,woff}')
+        .pipe($.flatten())
+        .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('extras', function () {
+    return gulp.src(['app/*.*', '!app/**/*.jade'], { dot: true })
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('check', function(){
+	return gulp.src('gulpfile.js')
+	    .pipe($.jshint())
+        .pipe($.jshint.reporter(require('jshint-stylish')));
+ });
+
+gulp.task('nodemon', function (cb) {
+    var started = false;
+    return $.nodemon({
+        script: 'index.js'
+    }).on('start', function () {
+        // to avoid nodemon being started multiple times
+        if (!started) {
+            cb();
+            started = true; 
+        }
+    });
+});
+
+gulp.task('serve', ['nodemon'], function() {
+    bsync.init(null, {
+        // server: "./dist"
+        proxy: "localhost:4000",
+        reloadDelay: 500
+    });
+    gulp.watch("app/sass/**/*.scss", ['styles']);
+    gulp.watch("app/scripts/**/*.js", ['scripts']);
+    gulp.watch("views/**/*.jade", reload);
+});
+
+gulp.task('build', ['clean'], function () {
+    gulp.start('build-full');
+});
+gulp.task('build-full', ['styles', 'scripts', 'images', 'fonts', 'extras']);
+
+gulp.task('default', ['serve']);
