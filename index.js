455b3cc7544edb4afb7ed619fbaf0d2de9703cad
Marcus Tan <marcus.m.tan@gmail.com>
2015-09-04 13:28:30 -0400

    different years for rex, events, nav-bar

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ var app = express();
 var path = require('path');
 var fs = require('fs');
 var execs = require('./json/execs');
-var events = require('./json/events/2014.json');
-var projects = require('./json/projects/2014.json');
 
 app.use('/vendor', express.static('./bower_components'));
 app.use('/assets', express.static('./dist'));
@@ -20,10 +18,24 @@ app.get('/', function (req, res) {
 });
 
 app.get('/rex', function (req, res) {
-  res.render('rex', { title: 'REX', projects: projects});
+  fs.readdir('./json/projects',function(err,years){
+    if(err) throw err;
+    var latest = years.slice(-1).toString().split('.')[0];
+    res.redirect('/rex/' + latest);
+  });  
 });
 
-app.get('/rex/:project', function (req, res) {
+app.get('/rex/:year', function (req, res) {
+  var year = req.params.year;
+  var render = require('./json/projects/' + year + '.json');
+  fs.readdir('./json/projects',function(err,years){
+    if(err) throw err;
+    res.render('rex', { title: 'REX', projects: render, years: years, thisYear: year});
+  });  
+});
+
+app.get('/rex/:year/:project', function (req, res) {
+  var projects = require('./json/projects/' + req.params.year + '.json');
   var id = req.params.project;
   projects.forEach(function(project){
     if (project.id == id){
@@ -33,8 +45,22 @@ app.get('/rex/:project', function (req, res) {
 });
 
 app.get('/events', function (req, res) {
-  res.render('events', { title: 'Events', events: events});
+  fs.readdir('./json/events',function(err,years){
+    if(err) throw err;
+    var latest = years.slice(-1).toString().split('.')[0];
+    res.redirect('/events/' + latest);
+  });
 });
+
+app.get('/events/:year', function (req, res) {
+  var year = req.params.year;
+  var render = require('./json/events/' + year + '.json');
+  fs.readdir('./json/events',function(err,years){
+    if(err) throw err;
+    res.render('events', { title: 'Events', events: render, years: years, thisYear: year});
+  });
+});
+
 
 app.get('/journal', function (req, res) {
   res.render('journal', { title: 'Undergraduate Journal'});
